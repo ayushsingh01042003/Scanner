3e99b3ff797d171d907e89faa32622c2c3331703
Ayush Singh <ayushsingh01042003@gmail.com>
2024-06-12 21:13:50 +0530

    Scrape the Frontend and add new daisyUI component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,41 +1,13 @@
-import React, { useState } from 'react';
-import FormContainer from './components/FormContainer';
-import './index.css';
+import React from 'react'
+import './index.css'
+import GitHubCard from './components/GitHubCard'
 
 const App = () => {
-  const [owner, setOwner] = useState('');
-  const [repo, setRepo] = useState('');
-  const [extensions, setExtensions] = useState([]);
-  const [keyValuePairs, setKeyValuePairs] = useState([]);
-  const [generatedJSON, setGeneratedJSON] = useState(null);
-  const [formError, setFormError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState(null);
-
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
-      <div className="bg-white rounded-lg shadow-lg p-8 max-w-2xl w-full">
-        <h1 className="text-4xl font-bold mb-8 text-blue-600 text-center">Data Upload</h1>
-        <FormContainer
-          owner={owner}
-          setOwner={setOwner}
-          repo={repo}
-          setRepo={setRepo}
-          extensions={extensions}
-          setExtensions={setExtensions}
-          keyValuePairs={keyValuePairs}
-          setKeyValuePairs={setKeyValuePairs}
-          formError={formError}
-          setFormError={setFormError}
-          loading={loading}
-          setLoading={setLoading}
-          response={response}
-          setResponse={setResponse}
-          setGeneratedJSON={setGeneratedJSON}
-        />
-      </div>
-    </div>
-  );
-};
+    <>
+      <GitHubCard />
+    </>
+  )
+}
 
-export default App;
+export default App
--- Frontend/src/components/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingMessage = () => (
-  <p className="text-lg font-semibold text-gray-700 mt-4">Loading...</p>
-);
-
-export default LoadingMessage;
