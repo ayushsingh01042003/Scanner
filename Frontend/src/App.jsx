bfab6d109919d42a0e400ca9396ede3036fd3a36
smk927 <shivamanik593@gmail.com>
2024-06-15 16:04:21 +0530

    GenReport called

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './index.css'
 import GenReport from './components/GenReport'
+import Report from './components/Report'
+import Navbar from './components/navbar'
+import SideBar from './components/sidebar'
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <GenReport/>
   )
 }
 
