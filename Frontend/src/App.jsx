f688a7152bdca04a04fe7be564c5de90ccc79511
smk927 <shivamanik593@gmail.com>
2024-06-23 18:54:44 +0530

    Made Changes - Added Reports Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import GenReport from './Pages/ReportsPage/ReportsPage';
+import ReportsPage from './Pages/Reports/ReportsPage';
+// import Overview from './Pages/Overview/Overview';
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="reports" element={<GenReport />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route path="overview" element={<Overview />} /> */}
+          <Route path="reports" element={<ReportsPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
