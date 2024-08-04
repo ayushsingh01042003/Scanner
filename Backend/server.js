7f5be3c4292de0e1afe7838c58f48c87664e1c18
ayushsingh01042003 <102677660+ayushsingh01042003@users.noreply.github.com>
2024-08-04 19:53:00 +0530

    Cleanup IP

--- Backend/server.js
+++ Backend/server.js
@@ -28,8 +28,6 @@ app.use(cookieParser())
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://192.168.56.1:5173',
-    'http://192.168.1.11:5173'
   ],
   credentials: true,
 }));
